bcebb7b36288cbad1164e5d313d15a9c30544a2a
rogiave12 <63801689+rogiave12@users.noreply.github.com>
2022-01-28 15:50:54 +0100

    new windows

---
+++ Password generator/password_input.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_generator
+{
+    /// <summary>
+    /// Interakční logika pro password_input.xaml
+    /// </summary>
+    public partial class password_input : Page
+    {
+        public password_input()
+        {
+            InitializeComponent();
+        }
+    }
+}
